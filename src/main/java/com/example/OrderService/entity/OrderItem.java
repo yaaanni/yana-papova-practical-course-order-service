c46dfb2dd737b4483dbdb70f0f81f02767ea1ca5
yaaanni <rw.yana.rw@gmail.com>
2026-01-28 16:50:37 +0300

    dao

---
+++ src/main/java/com/example/OrderService/entity/OrderItem.java
@@ -0,0 +1,42 @@
+package com.example.OrderService.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @PositiveOrZero
+    private Integer quantity;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "item_id")
+    private Item item;
+}
